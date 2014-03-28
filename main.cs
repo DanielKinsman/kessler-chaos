d40e1608b619c39a6ee9197636c5a47c3cffd28d
Daniel Kinsman <danielkinsman@riseup.net>
2014-03-28 11:36:01 +1100

    dotnet 3.5, start mod when in flight no on main menu

--- main.cs
+++ main.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace kesslerchaos
 {
-    [KSPAddon(KSPAddon.Startup.MainMenu,false)]
+    [KSPAddon(KSPAddon.Startup.Flight,false)]
     public class MBExtended : MonoBehaviourExtended
     {
         internal override void Awake()
@@ -18,19 +18,18 @@ namespace kesslerchaos
             StartRepeatingWorker(1);
         }
 
-        private Int32 intCounter=0;
         internal override void RepeatingWorker()
         {
-            System.Threading.Thread.Sleep(intCounter);
-            //increment the counter
-            intCounter += 1;
-            if (intCounter>9)
-            {
-                //Stop the function after ten seconds
-                StopRepeatingWorker();
-            }
-            LogFormatted("Last RepeatFunction Ran for: {0}ms",RepeatingWorkerDuration.TotalMilliseconds);
-            LogFormatted("UT Since Last RepeatFunction: {0}",RepeatingWorkerUTPeriod);
+			try
+			{
+	            LogFormatted("Last RepeatFunction Ran for: {0}ms",RepeatingWorkerDuration.TotalMilliseconds);
+	            LogFormatted("UT Since Last RepeatFunction: {0}",RepeatingWorkerUTPeriod);
+			}
+			catch(Exception e)
+			{
+				LogFormatted("You fucked up dan: {0}", e);
+				throw;
+			}
         }
     }
 
